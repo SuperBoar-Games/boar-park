119c8eab3981bceae926e02cdb6ac40198110346
Vamsi <vamsikalagaturu@gmail.com>
2025-03-22 23:41:10 +0100

    boar setup

---
+++ functions/db/schema.sql
@@ -0,0 +1,26 @@
+-- HEROES  TABLE
+CREATE TABLE heroes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE
+);
+
+-- MOVIES TABLE
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  hero_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  FOREIGN KEY (hero_id) REFERENCES heroes(id)
+);
+
+-- CARDS TABLE
+CREATE TABLE cards (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_id INTEGER, -- Nullable for add-on wild cards
+  hero_id INTEGER NOT NULL,
+  type TEXT NOT NULL CHECK (type IN ('HERO', 'VILLAIN', 'SR1', 'SR2', 'WC')),
+  call_sign TEXT, -- Nullable,
+  ability_text TEXT NOT NULL,
+  is_add_on BOOLEAN NOT NULL DEFAULT 0,
+  FOREIGN KEY (movie_id) REFERENCES movies(id),
+  FOREIGN KEY (hero_id) REFERENCES heroes(id)
+);
