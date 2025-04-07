8044410e1760ec75d75e5eb886ec0739e631863d
Vamsi <vamsikalagaturu@gmail.com>
2025-04-07 22:56:58 +0200

    added headers for /api

--- functions/api/utils.js
+++ functions/api/utils.js
@@ -1,13 +1,19 @@
 export const errorResponse = (message, status) => {
   return new Response(JSON.stringify({ error: message }), {
     status: status,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 };
 
 export const successResponse = (data) => {
   return new Response(JSON.stringify(data), {
     status: 200,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 };
